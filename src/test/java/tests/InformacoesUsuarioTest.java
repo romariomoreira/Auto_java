89426243aeec2645d5343a7cee9367b0a8f06f26
Romario Silva <romariomoreiradasilva@hotmail.com>
2018-10-24 00:46:00 -0300

    Removendo um contato, e aguardando 10 segundos para fechar a tela.

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -3,6 +3,7 @@ package tests;
 import static org.junit.Assert.*;
 
 
+import com.sun.org.apache.bcel.internal.ExceptionConstants;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +11,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,27 +22,20 @@ public class InformacoesUsuarioTest {
     private WebDriver navegador;
 
     @Before
-    public void setUp(){
+    public void setUp() {
 
         //Abrindo Navegador
         //Abaixo como é usado para passar o endereço da pasta onde está o navegador, caso esteja em pasta diferente do projeto.
         //System.setProperty("webdriver.chrome.driver","C:\\auto\\chromedriver.exe");
 
         //Abaixo como é usado para passar o navegador quando estiver na mesma pasta do Projeto.
-        System.setProperty("chrome.driver","chromedriver.exe");
-            navegador = new ChromeDriver();
-            navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        System.setProperty("chrome.driver", "chromedriver.exe");
+        navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-            //Navegando para site task
-            navegador.get("http://www.juliodelima.com.br/taskit");
+        //Navegando para site task
+        navegador.get("http://www.juliodelima.com.br/taskit");
 
-
-    }
-
-
-
-    @Test
-    public void testAdicionaruminformacaodousuario(){
         //Clicar no link que possui o texto "Sign in"
         navegador.findElement(By.linkText("Sign in")).click();
 
@@ -46,7 +43,7 @@ public class InformacoesUsuarioTest {
         //Identificando Formulário de Login
         WebElement formularioSingnInBox = navegador.findElement(By.id("signinbox"));
 
-        //Digitar no campo name "Login" que está dentro do formulário de ID "signinbox" o texto "julio0001"
+        //Digitar no campo name "Login" que está dentro do formulário de ID "signinbox" o texto "julio0001"      acaodousuario() {
         formularioSingnInBox.findElement(By.name("login")).sendKeys("julio0001");
 
         //Digitar no campo name "Password" que está dentro do formulário de ID "signinbox" o texto "123456"
@@ -63,6 +60,14 @@ public class InformacoesUsuarioTest {
         //Clicar em um link que possui o texto "MORE DATA ABOUT YOU"
         navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
 
+    }
+
+
+
+
+      //@Test
+      public void adicionarInformacaodoUsuario() {
+
         //Clicar no botão atráves do seu xpath //button[@data-target="addmoredata"]
         navegador.findElement(By.xpath("//button[@data-target=\'addmoredata\']")).click();
 
@@ -70,8 +75,8 @@ public class InformacoesUsuarioTest {
         WebElement popaddmoredata = navegador.findElement(By.id("addmoredata"));
 
         //No combo de name "type" escolhe a opção "Phone"
-       WebElement CampoType =  popaddmoredata.findElement(By.name("type"));
-       new Select(CampoType).selectByVisibleText("Phone");
+        WebElement CampoType = popaddmoredata.findElement(By.name("type"));
+        new Select(CampoType).selectByVisibleText("Phone");
 
         // No campo de name "Contact" digitar "+5511933293399"
         popaddmoredata.findElement(By.name("contact")).sendKeys("+5511933293399");
@@ -82,29 +87,55 @@ public class InformacoesUsuarioTest {
         // Na mensagem de id "toast-container" validar que o texto é "Your contact has been added!"
         WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
         String mensagem = mensagemPop.getText();
-        assertEquals("Your contact has been added!",mensagem);
+        assertEquals("Your contact has been added!", mensagem);
+    }
 
 
 
+    @Test
+    public void removerUmContatoDoUsuario() {
+    // Clicar no elemento pelo seu Xpath //span[text()="+5511933293399"]/following-sibling::a
+        navegador.findElement(By.xpath("//span[text()=\"+5511933293399\"]/following-sibling::a")).click();
 
+    // Confirmar a janela JavaScrit
+        navegador.switchTo().alert().accept();
+
+    //Validar que a mensagem  apresrntada foi Rest in peace, dear phone!
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals("Rest in peace, dear phone!", mensagem);
+        
+    //Agaurdar até 10 segundos para a janela desapareça
+        WebDriverWait aguardar = new WebDriverWait(navegador, 10)  ;
+         aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
+
+    // Clicar no texto "Logout"
+        navegador.findElement(By.linkText("Logout")).click();
 
     }
+
+
+
 
 @After
 
     public void tearDown(){
 //Fechar o navegador
 
-    navegador.quit();
-    //assertEquals(1,1);
+   navegador.quit();
 
-}
-
-
-}
+    }           }
+    
 
 
-//Validar que dentro do elemento com class "me" está o texto "Hi, Julio"
-// WebElement me = navegador.findElement(By.className("me"));
-//String TextoElementoME = me.getText();
-//assertEquals("Hi, Julio",TextoElementoME);
+
+
+
+
+
+
+
+
+
+
+
